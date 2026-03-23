0dd6f4e245b14993044f863f88cd2f44d36d1ab4
320-25\ymy0807 <ymylcy8094@gmail.com>
2026-03-23 13:26:36 +0900

    fix : 도(道)와주세요 비로그인 유저 접속 불가 추가

--- src/service/ai_model_service.py
+++ src/service/ai_model_service.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
+from src.common import login_required
 
 model_bp = Blueprint('model', __name__)
 
 @model_bp.route('/', methods=['GET', 'POST'])
+@login_required
 def get_tip_data():
     # 여기서 필요한 데이터 처리를 할 수 있습니다.
     return render_template('ai_model/model.html')
